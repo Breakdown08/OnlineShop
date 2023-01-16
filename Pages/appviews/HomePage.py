4c296bca9a069ec45420e9930e01f2913743d855
Breakdown08 <nicegameafk@gmail.com>
2023-01-16 16:43:08 +0700

    категории

--- Pages/appviews/HomePage.py
+++ Pages/appviews/HomePage.py
@@ -1,19 +1,21 @@
 from ..utils import *
+from pprint import pprint
+
+def fill_categories():
+    all_categories = []
+    main_categories = Category.objects.all()
+    for main_category in main_categories:
+        child_categories = []
+        items = ChildCategory.objects.filter(parent_category_id=main_category.id)
+        child_categories.append({'items': items, 'count': items.count()})
+        all_categories.append({'key': main_category.title, 'slug': main_category.slug, 'child_categories': child_categories, 'count': len(child_categories)})
+    return all_categories
 
 def homepage(request):
     data = {}
-    all_categories = []
-    #news = New.objects.all().filter(is_private=False)
-    main_categories = Category.objects.all()
-    for item in main_categories:
-        child_categories = []
-        child_categories.append({'value': ChildCategory.objects.filter(parent_category_id=item.id), 'products_count': None})
-        # for category in child_categories['value']:
-        #     products_count = Product.objects.filter(category_id=category.id).count()
-
-        all_categories.append({'key': item.title, 'slug': item.slug, 'value': child_categories, 'childs_count': 2})
+    all_categories = fill_categories()
     data.update({'all_categories': all_categories})
-    print(data)
+    pprint(data)
     return render(request, 'Pages/HomePage.html', context=data)
 
 
