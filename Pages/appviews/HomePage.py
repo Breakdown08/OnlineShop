8d1026c094d1d1b59ea7fe14a38820421631af55
Breakdown08 <nicegameafk@gmail.com>
2023-01-17 02:37:10 +0700

    homepage

--- Pages/appviews/HomePage.py
+++ Pages/appviews/HomePage.py
@@ -5,16 +5,19 @@ def fill_categories():
     all_categories = []
     main_categories = Category.objects.all()
     for main_category in main_categories:
-        child_categories = []
-        items = ChildCategory.objects.filter(parent_category_id=main_category.id)
-        child_categories.append({'items': items, 'count': items.count()})
-        all_categories.append({'key': main_category.title, 'slug': main_category.slug, 'child_categories': child_categories, 'count': len(child_categories)})
+        child_categories = ChildCategory.objects.filter(parent_category_id=main_category.id)
+        all_categories.append({'key': main_category.title, 'slug': main_category.slug, 'child_categories': child_categories, 'count': child_categories.count()})
     return all_categories
+
+
+def get_last_news():
+    news = New.objects.all()[:3]
 
 def homepage(request):
     data = {}
     all_categories = fill_categories()
-    data.update({'all_categories': all_categories})
+    news = get_last_news()
+    data.update({'all_categories': all_categories, 'news': news})
     pprint(data)
     return render(request, 'Pages/HomePage.html', context=data)
 
